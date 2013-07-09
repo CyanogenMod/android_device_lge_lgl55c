8961b75489cd01f09dcfd9cce153a07c54c98c7e
stedman420 <stedmanchris420@gmail.com>
2013-07-08 23:10:16 -0700

    small fix

--- cm.mk
+++ cm.mk
@@ -1,8 +1,11 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/cdma.mk)
+
 # Release name
 PRODUCT_RELEASE_NAME := lge_gelato
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cyanogen/products/common.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/lgl55c/full_lgl55c.mk)
